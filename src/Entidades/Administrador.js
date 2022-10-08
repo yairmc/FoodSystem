74aaad129d08524bcadbeceb859a9c2574f1ab8b
Manuel de Jesua Valenzuela Vazquez <manuel.valenzuela216785@potros.itson.edu.mx>
2022-10-08 15:57:54 -0700

    Cambio de idioma

--- src/Entidades/Administrador.js
+++
@@ -1,7 +0,0 @@
-import { Usuario } from "./Usuario.js"
-
-export class Administrador extends Usuario{
-    constructor(nombre, username, password){
-        super(nombre, username, password)
-    }
-}
--- src/Entidades/Mesero.js
+++
@@ -1,7 +0,0 @@
-import { Usuario } from "./Usuario.js"
-
-export class Mesero extends Usuario{
-    constructor(nombre, username, password){
-        super(nombre, username, password)
-    }
-}
