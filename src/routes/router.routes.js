0b11a68884ba497d91d1b5cd5de49c9359c819fe
Orlando Camacho <soy.orlandocg@gmail.com>
2022-10-17 19:15:06 -0700

    Controller

--- src/routes/router.routes.js
+++ src/routes/router.routes.js
@@ -1,9 +1,11 @@
 import express from "express";
 const RouterApp = express.Router();
 import userRouter from "./user.routes.js";
-import authRouter from "./auth.routes.js"
+import authRouter from "./auth.routes.js";
+import roleRouter from "./role.routes.js";
 
 RouterApp.use(userRouter);
 RouterApp.use(authRouter);
+RouterApp.use(roleRouter);
 
 export default RouterApp;
