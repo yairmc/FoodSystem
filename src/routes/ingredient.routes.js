b6a444feab7605e7e9953ac565234d3033ffa24e
Manuel De Jesus Valenzuela Vazquez <89809900+ManuelVzla03@users.noreply.github.com>
2022-10-18 10:20:31 -0700

    Fixed rout ingredients.

--- src/routes/ingredient.routes.js
+++ src/routes/ingredient.routes.js
@@ -5,7 +5,7 @@ const ingredientRouter = express.Router();
 const { addIngredient, updateIngredient, getAllIngredients, getIngredientById, getIngredientByName, deleteIngredient } = ingredientController;
 ingredientRouter.get("/ingredients", getAllIngredients);
 ingredientRouter.post("/ingredients", addIngredient);
-ingredientRouter.get("/ingredients/:name", getIngredientByName);
+ingredientRouter.get("/ingredients/name", getIngredientByName);
 ingredientRouter.get("/ingredients/:id", getIngredientById);
 ingredientRouter.delete("/ingredients/:id", deleteIngredient);
 ingredientRouter.put("/ingredients/:id", updateIngredient);
