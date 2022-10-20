0e2178fa417e1c291e526c6c4adbaf81305b9040
yairmc <yairmc666@gmail.com>
2022-10-20 15:54:40 -0700

    Integrate tableController in routes

--- src/routes/table.routes.js
+++ src/routes/table.routes.js
@@ -1,12 +1,13 @@
 import express from 'express';
+import { addTable, updateTable, deleteTable, getTableById, getTableByNumber, getAllTables } from '../controllers/tableController';
 
 const tableRouter = express.Router();
 
-tableRouter.get('/tables')
-tableRouter.post('/tables')
-tableRouter.get('/tables/:id')
-tableRouter.get('/tables/:number')
-tableRouter.delete('tables/:id')
-tableRouter.put('tables/:id')
+tableRouter.get('/tables', getAllTables)
+tableRouter.post('/tables', addTable)
+tableRouter.get('/tables/:id', getTableById)
+tableRouter.get('/tables/:number', getTableByNumber)
+tableRouter.delete('tables/:id', deleteTable)
+tableRouter.put('tables/:id', updateTable)
 
 export default tableRouter;
