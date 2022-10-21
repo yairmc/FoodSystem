07b7933539a3d10470acd42ef966b621f099e8f8
yairmc <yairmc666@gmail.com>
2022-10-21 01:41:29 -0700

    Create tableController.test.js

---
+++ test/controller/tableControlles.test.js
@@ -0,0 +1,14 @@
+import { app } from "../../src/main";
+import supertest from "supertest";
+import { expect } from '@jest/globals'
+describe("Testing tableController", () => {
+
+    test('must add new Table', () => { second })
+    test('must throw an error when we dont created a table thit a empty data', () => { second })
+    test('must return all tables', () => { second })
+    test('must return table by id', () => { second })
+    test('must return table by number', () => { second })
+    test("must throw an error if the table doesn't exist", () => { second })
+    test('must updated table by id', () => { second })
+    test('must deleted table by id', () => { second })
+})
