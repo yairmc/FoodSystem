3a52d19fae2004bab5b3f5f10b12f0128baa23d8
DianaLauraMN <dianaalauramn@gmail.com>
2022-10-21 18:33:24 -0700

    Feature/FS-26 #comment Product Controller was created and Rroduct Repository was added to Product Controller

--- src/controllers/prodController.js
+++ src/controllers/prodController.js
@@ -1,1 +1,56 @@
 import ProductRepository from "../repositories/Products.repository.js";
+const prodRepository = new ProductRepository();
+
+const addProduct = async (product) => {
+    try {
+        const prodAdded = await prodRepository.create(product);
+        return prodAdded;
+    } catch (error) {
+        return error;
+    }
+};
+
+const getAllProducts = async () => {
+    try {
+        const products = await prodRepository.findAll();
+        return products;
+    } catch (error) {
+        return error;
+    }
+};
+
+const getProductById = async (id) => {
+    try {
+        const product = await prodRepository.findOne(id);
+        return product;
+    } catch (error) {
+        return error;
+    }
+};
+
+const getProductByName = async (name) => {
+    try {
+        const product = await prodRepository.findByName(name);
+        return product;
+    } catch (error) {
+        return error;
+    }
+};
+
+const updateProduct = async (id, product) => {
+    try {
+        const productUpdated = await prodRepository.update(id, product);
+        return productUpdated;
+    } catch (error) {
+        return error
+    }
+}
+const deleteProduct = async (id) => {
+    try {
+        const prodDeleted = await prodRepository.delete(id);
+        return prodDeleted;
+    } catch (error) {
+        return error;
+    }
+}
+export { addProduct, getAllProducts, getProductById, getProductByName, updateProduct, deleteProduct };
--- src/repositories/Products.repository.js
+++ src/repositories/Products.repository.js
@@ -4,13 +4,21 @@ import { ProductModel } from '../models/Product.model.js';
 export default class ProductRepository {
 
     async create(product) {
-        const productAux = new product.toPersistenceObject();
+        const productAux = product.toPersistenceObject();
         const prodCreated = await ProductModel.create(productAux);
         return new Product(prodCreated.name, prodCreated.basePrice, prodCreated.cost, prodCreated.availability, prodCreated.taxes, prodCreated.type, prodCreated.stock);
     }
 
     async update(id, product) {
-        const prodUpdated = await ProductModel.update({ name: product.name, basePrice: product.basePrice, cost: product.cost, availability: product.availability, taxes: product.taxes, type: product.typem, stock: product.stock }, {
+        const prodUpdated = await ProductModel.update({
+            name: product.name,
+            basePrice: product.basePrice,
+            cost: product.cost,
+            availability: product.availability,
+            taxes: product.taxes,
+            type: product.type,
+            stock: product.stock
+        }, {
             where: {
                 id
             }
@@ -36,11 +44,20 @@ export default class ProductRepository {
         return product;
     }
 
-    async findAll() {
-        const products = await RoleModel.findAll({
-            order: ['id'],
-            attributes: ['name', 'basePrice', 'cost', 'availability', 'taxes', 'type', 'stock']
+    async findByName(name) {
+        const product = await ProductModel.findOne({
+            where: {
+                name
+            }
         });
-        return products;
+        return product;
+    };
+
+    async findAll() {
+        const products = await ProductModel.findAll({
+            order: ['id'],
+            attributes: ['id', 'name', 'basePrice', 'cost', 'availability', 'taxes', 'type', 'stock']
+        });
+        return products.map(iterator => iterator.dataValues);
     }
 }
