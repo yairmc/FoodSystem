68d25cf34a7591a5ccf64cc8663c673b2315a610
yairmc <yairmc666@gmail.com>
2022-10-23 11:33:27 -0700

    commit for pull changes

---
+++ src/controllers/ticketController.js
@@ -0,0 +1,60 @@
+import TicketRepository from '../repositories/Tickets.repository.js'
+const ticketRepository = new TicketRepository();
+
+const getAllTicket = async (req, res) => {
+    try {
+        const tickets = await ticketRepository.findAllTickets();
+        res.status(200).json(tickets)
+    } catch (error) {
+        return res.status(500).json({ msg: "Error while queryng all tickets" })
+    }
+}
+const getTicketById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const ticket = await ticketRepository.findOneTicketById(id);
+        if (!ticket) return res.status(404).json({ msg: "This ticket doesn't exist" });
+    } catch (error) {
+        res.status(500).json({ msg: "Error while queryng ticket" })
+    }
+}
+
+const getTicketByDate = (req, res) => {
+
+}
+
+const getTicketByOrder = (req, res) => {
+
+}
+
+const addTicket = async (req, res) => {
+    try {
+        const ticketAux = await ticketRepository.createTicket(req.body);
+        res.status(200).json(ticketAux);
+    } catch (error) {
+        res.status(500).json({ msg: "Error while adding tickets" })
+    }
+}
+
+const updatedTicket = async (req, tes) => {
+    try {
+        const { id } = req.params;
+        await ticketRepository.updateTicket(id, req.body);
+        res.status(200).json({ msg: "Ticket updated" })
+    } catch (error) {
+        return res.status(500).json({ msg: "Error while updating ticket" });
+    }
+}
+
+const deleteTicket = async (req, res) => {
+    try {
+        const { id } = req.params;
+        await ticketRepository.deleteTicket(id);
+        res.status(200).json({ msg: "Ticket deleted" });
+    } catch (error) {
+        res.status(500).json({ msg: "Error while deleting ticket" })
+    }
+
+}
+
+export { addTicket, getAllTicket, getTicketByDate, getTicketById, getTicketByOrder, updatedTicket, deleteTicket }
