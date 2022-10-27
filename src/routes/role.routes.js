a5d764a3aef2c9ebacb8a192f343e350884c72bb
yairmc <yairmc666@gmail.com>
2022-10-26 19:23:11 -0700

    roles.validatos.js was created like a midelware

--- src/routes/role.routes.js
+++ src/routes/role.routes.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { roleController } from "../controllers/index.js";
+import { validateAddRole, validateDeleteRole, validateGetRoleById, validateUpdateRole } from "../validators/role.validator.js";
 const roleRouter = express.Router();
 
 const { addRole, deleteRole, getAllRoles, getRoleById, updateRole } = roleController;
 roleRouter.get("/roles", getAllRoles);
-roleRouter.post("/roles", addRole);
-roleRouter.get("/roles/:id", getRoleById);
-roleRouter.delete("/roles/:id", deleteRole);
-roleRouter.put("/roles/:id", updateRole);
+roleRouter.post("/roles", validateAddRole(), addRole);
+roleRouter.get("/roles/:id", validateGetRoleById(), getRoleById);
+roleRouter.delete("/roles/:id", validateDeleteRole(), deleteRole);
+roleRouter.put("/roles/:id", validateUpdateRole(), updateRole);
 
 export default roleRouter;
