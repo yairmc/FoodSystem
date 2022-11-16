73184766471642e7db0e6cc2b095f9622892ddf1
Miguel Ciulog <angel.ciulog7@gmail.com>
2022-11-15 19:03:59 -0700

    Finished tests on auth

--- test/routes/auth.routes.test.js
+++ test/routes/auth.routes.test.js
@@ -6,34 +6,64 @@ describe("FS-8 Test the authentication paths", () => {
     const request = supertest(app);
 
     const user = {
-        name: "Miguel",
         userName: "Akuasnin",
+        password: "1234567890",
+    };
+
+    const invalidUser = {
+        userName: "",
+        password: "fooooooobaaar",
+    };
+
+    const badUser = {
+        userName: "Bruh",
+        password: "100000000000",
     };
 
     test("Validate that the user exists and the token is returned.", async () => {
-        const { body: dbUser } = await request.get(
-            `/users/username?username=${user.userName}`
-        );
-
-        expect(dbUser._name).toBe(user.name);
-
-        var { body: tokenResponse } = await request.post("/auth").send(user);
-        expect(tokenResponse.accessToken).toBeDefined()
+        const {
+            body: tokenResponse,
+            status: statusResponse
+        } = await request.post("/auth").send(user);
+        expect(tokenResponse.token).toBeDefined()
+        expect(tokenResponse.msg).toBeUndefined()
+        expect(statusResponse).toBe(200)
     });
 
-    // TODO: The jwt token generation does not check beforehand if the user does indeed exist.
-    // test('Validate that it returns an error if the user trying to authenticate does not exist', async () => { second })
+    test(
+        "Validate that it returns an error if the user trying to authenticate does not exist", async () => {
+        const {
+            body: tokenResponseBad,
+            status: statusResponseBad
+        } = await request.post("/auth").send(badUser);
+        expect(tokenResponseBad.token).toBeUndefined()
+        expect(tokenResponseBad.msg).toBeDefined()
+        expect(statusResponseBad).toBe(404)
+    });
 
-    // TODO: The jwt token generation does not check beforehand if the user does indeed exist.
-    // test('Validate that it returns an error in case of being an invalid user.', async () => { second })
+    test('Validate that it returns an error in case of being an invalid user.', async () => {
+        const {
+            body: tokenResponseBad,
+            status: statusResponseBad
+        } = await request.post("/auth").send(invalidUser);
+
+        expect(tokenResponseBad.token).toBeUndefined()
+        expect(tokenResponseBad.msg).toBeDefined()
+        expect(statusResponseBad).toBe(404)
+    })
 
     test('Validate that the token is correct.', async () => {
-        const { body } = await request.post("/auth").send(user);
-        let authorization = await request.get("/auth").set(
-            'authorization', body
-        ).send()
+        const {
+            body: tokenResponse,
+            status: statusResponse
+        } = await request.post("/auth").send(user);
+        expect(statusResponse).toBe(200)
 
-        expect(authorization.status).toBe(200);
+        const { status: objStatus } = await request.get("/users").set(
+            "authorization",
+            `Bearer ${tokenResponse.token}`
+        );
+        expect(objStatus).toBe(200);
     })
 });
 
